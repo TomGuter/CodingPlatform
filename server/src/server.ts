f6cb1c4c6139a337f67e7496854dddb4e9fd406a
Tom Guter <128406348+TomGuter@users.noreply.github.com>
2025-03-19 14:48:30 +0200

    prod fixes

--- server/src/server.ts
+++ server/src/server.ts
@@ -45,9 +45,9 @@ const initializeServer = async (): Promise<{ app: Express; io: Server; server: R
     app.use('/codeBlocks', codeblocks_route);
 
     if (process.env.NODE_ENV === 'production') {
-      app.use(express.static(path.join(__dirname, 'webApp', 'build')));
+      app.use(express.static(path.join(__dirname, 'webApp'))); 
       app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'webApp', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'webApp', 'index.html'));
       });
     }
 
